59b3728d43807170669bc8769a5b116fbf12a4f2
Blastonn <felipe.paulo07@hotmail.com>
2025-06-25 11:16:35 -0300

    Feat - Adicionar funcao para mostrar os livros
    Adicionada a funcao para exibir os livros na
    biblioteca.

--- script.js
+++ script.js
@@ -5,14 +5,33 @@ function Book(title, author){
     this.author = author;
 }
 
-function addBookToLibrary(title,author){
-    const book = new Book(title, author);
+function addBookToLibrary(book){
     book.id = crypto.randomUUID();
     myLibrary.push(book);
     return book;
 }
 
-addBookToLibrary("Harry Potter","JK Rowling");
+function showBook(book) {
+    const containerCard = document.querySelector(".container-card")
+    const card = document.createElement("div")
+    card.classList.add("card");
+    card.textContent = `${book.title} ${book.author}`;
+    containerCard.appendChild(card);
+}
+
+const book1 = new Book("Harry Potter", "J.k Rowling");
+const book2 = new Book("title", "author");
+const book3 = new Book("title", "author");
+
+
+
+
+addBookToLibrary(book1);
+showBook(book1);
+addBookToLibrary(book2);
+showBook(book2);
+addBookToLibrary(book3);
+showBook(book3);
 
 
 console.log(myLibrary);
